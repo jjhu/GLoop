bc9ea73eb5ef4ea383b4599acd977368e0d2d099
Javier Soto <javiersoto15@users.noreply.github.com>
2019-04-05 19:40:08 -0500

    Updates 04/05:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RemoveIonIconSvgStrokeDirective } from './directives/remove-ion-icon-svg-stroke.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RemoveIonIconSvgStrokeDirective],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
---
+++ src/app/directives/remove-ion-icon-svg-stroke.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RemoveIonIconSvgStrokeDirective } from './remove-ion-icon-svg-stroke.directive';
+
+describe('RemoveIonIconSvgStrokeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RemoveIonIconSvgStrokeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
